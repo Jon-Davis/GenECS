d0b8215fb72690e2d092f845a50d9d424c843262
Jon Davis <JonathonDevinDavis@gmail.com>
2019-03-26 13:59:50 -0700

    Systems now use scoped threads

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,4 @@ pub mod entity;
 #[macro_use] pub mod system;
 #[macro_use] pub mod resource;
 #[macro_use] pub mod static_storage;
+pub extern crate crossbeam_utils;
--- src/system.rs
+++ src/system.rs
@@ -6,30 +6,19 @@ pub trait System {
 
 /// the dispatch! macro is used 
 #[macro_export] macro_rules! dispatch {
-    ([$($system:tt),*]) => {
-        // launch threads and recurse on all repitions, collect all threadIDs into an array handles
-        let mut handles = [$(Some(std::thread::spawn(move || {dispatch!($system); })),)*];
-        // join all the threads
-        for handle in &mut handles {
-            // since JoinHandles require ownership to join, and we cant take ownership out
-            // of an array without creating undefined memory, we us an Option<JoinHandle> 
-            // and the take method to gain ownership of the JoinHandles.
-            let handle = handle.take();
-            match handle {
-                Some(handle) => handle.join().unwrap(),
-                _ => unreachable!(),
-            };
-        }
+    ($($system:expr),*) => {
+        $($system.run();)*
     };
-    (($($system:tt),*)) => {
-        // recurse on repetition
-        $(dispatch!($system);)*
-    };
-    ($system:tt) => {
-        $system.run()
-    };
-    ($($system:tt),*) => {
-        // recurse on repetition
-        $(dispatch!($system);)*
+}
+
+#[macro_export] macro_rules! dispatch_parallel {
+    ($($system:expr),*) => {
+        genecs::crossbeam_utils::thread::scope(|s| {
+            $(
+                s.spawn(|_| {
+                    $system.run();
+                });
+            )*
+        }).unwrap();
     };
 }
